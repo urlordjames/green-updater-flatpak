3b311cba279efd43d26443b6aa7dfbc3b7b033ef
urlordjames <32751441+urlordjames@users.noreply.github.com>
2024-07-12 13:33:49 -0400

    quit if command failed in python script

--- ci-build.py
+++ ci-build.py
@@ -3,11 +3,17 @@
 import json
 import os
 
+def run(cmd):
+	result = os.system(cmd)
+	exit_code = os.waitstatus_to_exitcode(result)
+	if exit_code != 0:
+		exit()
+
 with open("io.github.urlordjames.GreenUpdater.json", "r") as f:
 	parsed = json.load(f)
 	commit = parsed["modules"][0]["sources"][0]["commit"]
 
-os.system("git clone 'https://github.com/urlordjames/green-updater.git'")
-os.system(f"cd green-updater && git checkout {commit} && cd ..")
-os.system("flatpak-cargo-vendor green-updater/Cargo.lock -o cargo-source.json")
-os.system("./common-build.sh")
+run("git clone 'https://github.com/urlordjames/green-updater.git'")
+run(f"cd green-updater && git checkout {commit} && cd ..")
+run("flatpak-cargo-vendor green-updater/Cargo.lock -o cargo-source.json")
+run("./common-build.sh")
