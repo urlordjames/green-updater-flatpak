1aab4bdd4aca49be01e64f71b99d67b0d04dcd07
urlordjames <32751441+urlordjames@users.noreply.github.com>
2024-07-12 13:39:38 -0400

    download flatpak-cargo-generator in ci build

--- ci-build.py
+++ ci-build.py
@@ -15,5 +15,7 @@ with open("io.github.urlordjames.GreenUpdater.json", "r") as f:
 
 run("git clone 'https://github.com/urlordjames/green-updater.git'")
 run(f"cd green-updater && git checkout {commit} && cd ..")
-run("flatpak-cargo-vendor green-updater/Cargo.lock -o cargo-source.json")
+run("git clone 'https://github.com/flatpak/flatpak-builder-tools.git'")
+run("cd flatpak-builder-tools && git checkout 338ce9c6512d49d98ae9a508d219ffe19b5144e8 && cd ..")
+run("python3 flatpak-builder-tools/cargo/flatpak-cargo-generator.py green-updater/Cargo.lock -o cargo-source.json")
 run("./common-build.sh")
