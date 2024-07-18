ee794a68387e0e022d9b9244ed741ed2146ff8fd
urlordjames <32751441+urlordjames@users.noreply.github.com>
2024-07-18 14:21:22 -0400

    bump version

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
 					owner = "urlordjames";
 					repo = "green-updater";
 					rev = green-updater-commit;
-					sha256 = "sha256-ibWXCfD7M/WYTCop6s/KTSd4G6dxxyvHRyEzrxXjZf8=";
+					sha256 = "sha256-FPnTmC0i6Q1W1aMPo4VfHT2j63nOCGDlcUb/dD5ixmg=";
 				};
 
 				buildPhase = "mkdir $out && cp Cargo.lock $out/Cargo.lock";
